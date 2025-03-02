b83b20fd013f33a30fdae253912b0e7348598052
bazimR <rishaalmuhammed@gmail.com>
2025-03-02 11:19:43 +0530

    presist nav data

--- Navigation-extras/ContentView.swift
+++ Navigation-extras/ContentView.swift
@@ -7,15 +7,63 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+@Observable
+class PathStore {
+    var path: NavigationPath {
+        didSet {
+            save()
         }
-        .padding()
+    }
+    let savedPath = URL.documentsDirectory.appending(path: "SavedPath")
+    init() {
+        if let data = try? Data(contentsOf: savedPath) {
+            if let decoded = try? JSONDecoder().decode(
+                NavigationPath.CodableRepresentation.self,
+                from: data
+            ) {
+                path = NavigationPath(decoded)
+                return
+            }
+        }
+        path = NavigationPath()
+    }
+
+    func save() {
+        guard let representation = path.codable else { return }
+        do {
+            let data = try JSONEncoder().encode(representation)
+            try data.write(to: savedPath)
+        } catch {
+            print("Failed to save navigation data.")
+        }
+    }
+}
+
+struct DetailView: View {
+    var number: Int
+    @Binding var path: NavigationPath
+    var body: some View {
+        NavigationLink("Go to Random Number", value: Int.random(in: 1...1000))
+            .navigationTitle("Number: \(number)").toolbar {
+                Button {
+                    path = NavigationPath()
+                } label: {
+                    Text("Home")
+                }
+
+            }
+    }
+}
+
+struct ContentView: View {
+    @State private var path = PathStore()
+    var body: some View {
+        NavigationStack(path: $path.path) {
+            DetailView(number: 0, path: $path.path)
+                .navigationDestination(for: Int.self) { i in
+                    DetailView(number: i, path: $path.path)
+                }
+        }
     }
 }
 
